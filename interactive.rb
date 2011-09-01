74f06cd4b27ddb76a467b04316157094c1bb7833
Tim Ekl <lithium3141@gmail.com>
2011-09-01 13:07:21 -0400

    Add easy-POST function to interactive test script

--- interactive.rb
+++ interactive.rb
@@ -3,3 +3,10 @@ Dir.glob('./models/*.rb').each { |f| require f }
 DataMapper.setup(:default, "sqlite::memory:")
 DataMapper.auto_upgrade!
 DataMapper.finalize
+
+require 'uri'
+require 'net/http'
+def post(uri, data = {})
+  res = Net::HTTP.post_form(URI.parse(uri), data)
+  return res.body
+end
