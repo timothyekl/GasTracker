ca43d381191a7f42edd9d4178c22ceff3ea5c600
Tim Ekl <lithium3141@gmail.com>
2011-09-02 10:38:33 -0400

    Add drivers list page, creation route

--- routes/driver.rb
+++ routes/driver.rb
@@ -1,9 +1,30 @@
 class GasTracker
+  get '/drivers' do
+    haml :drivers
+  end
+
   post '/get/drivers' do
     Driver.all.to_json
   end
   
   post '/get/driver/:id' do
     Driver.first(:id => params[:id]).to_json
+  end
+
+  post '/add/driver' do
+    name = params[:name]
+
+    driver = Driver.create(
+        :name => name
+    )
+    if !driver.save
+      return {:success => false, :error => "Could not save driver"}.to_json
+    end
+    
+    if params[:redirect].nil?
+      return {:success => true}.to_json
+    else
+      redirect params[:redirect]
+    end
   end
 end
