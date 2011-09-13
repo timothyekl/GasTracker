6b528f4f5ce47172994e6ef704ed32935401c025
Tim Ekl <lithium3141@gmail.com>
2011-09-13 13:54:44 -0400

    Refactor out graphs to separate routes file; make graph width-configurable

---
+++ routes/graphs.rb
@@ -0,0 +1,26 @@
+require 'gruff'
+
+class GasTracker
+
+  get '/graph/gas_cost_over_time.png' do
+    width = 280
+    width = params[:width].to_i if !(params[:width].nil?)
+
+    g = Gruff::Line.new(width)
+    g.title = "Gas cost over time"
+
+    cost_data = Purchase.all.map{|p| p.cost_per_gallon.round(2)}
+
+    g.minimum_value = cost_data.min.floor
+    g.maximum_value = cost_data.max.ceil
+    g.data("Cost", cost_data, '#96f')
+    g.hide_dots = true
+    g.hide_legend = true
+    g.theme = g.theme_greyscale
+    
+    path = './tmp/gas_cost_over_time.png'
+    g.write(path)
+    send_file path, :type => "image/png", :disposition => "inline"
+  end
+
+end
--- routes/snippets.rb
+++ routes/snippets.rb
@@ -1,22 +1,7 @@
-require 'gruff'
-
 class GasTracker
 
   get '/stats' do
     haml :stats
-  end
-
-  get '/graph/gas_cost_over_time.png' do
-    g = Gruff::Line.new
-    g.title = "Gas cost over time"
-
-    cost_data = Purchase.all.map{|p| p.cost_per_gallon}
-
-    g.data("Cost", cost_data)
-    
-    path = './tmp/gas_cost_over_time.png'
-    g.write(path)
-    send_file path, :type => "image/png", :disposition => "inline"
   end
 
 end
