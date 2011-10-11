8975cf8354b13f5d30bbda9b3704d818e1efb6c6
Tim Ekl <lithium3141@gmail.com>
2011-10-11 12:15:45 -0400

    Move validations into dm-validations, as opposed to doing them manually

--- models/purchase.rb
+++ models/purchase.rb
@@ -10,6 +10,10 @@ class Purchase
   belongs_to :driver
   belongs_to :car
 
+  validates_numericality_of :miles, :gte => 0.0
+  validates_numericality_of :gallons, :gte => 0.0
+  validates_numericality_of :cost, :gte => 0.0
+
   def miles_per_gallon
     return self.miles / self.gallons
   end
--- routes/purchase.rb
+++ routes/purchase.rb
@@ -11,7 +11,7 @@ class GasTracker
     end
 
     driver_id = params[:driver].to_i
-    if !driver_id.nil? && driver_id.to_s == params[:driver]
+    if !driver_id.nil?
       driver = Driver.first(:id => driver_id)
       if !driver.nil?
         conds[:driver] = driver
@@ -39,33 +39,18 @@ class GasTracker
     end
 
     gallons = params[:gallons].to_f
-    if gallons.to_s != params[:gallons]
-      return {:success => false, :error => "Could not parse gallons"}.to_json
-    end
 
     miles = params[:miles].to_f
-    if miles.to_s != params[:miles]
-      return {:success => false, :error => "Could not parse miles"}.to_json
-    end
 
     cost = params[:cost].to_f
-    if cost.to_s != params[:cost]
-      return {:success => false, :error => "Could not parse cost"}.to_json
-    end
 
     car_id = params[:car].to_i
-    if car_id.to_s != params[:car]
-      return {:success => false, :error => "Could not parse car ID"}.to_json
-    end
     car = Car.first(:id => car_id)
     if car.nil?
       return {:success => false, :error => "Could not find car with ID %{id}" % {:id => car_id}}.to_json
     end
 
     driver_id = params[:driver].to_i
-    if driver_id.to_s != params[:driver]
-      return {:success => false, :error => "Could not parse driver ID"}.to_json
-    end
     driver = Driver.first(:id => driver_id)
     if driver.nil?
       return {:success => false, :error => "Could not find driver with ID %{id}" % {:id => driver_id}}.to_json
@@ -92,10 +77,6 @@ class GasTracker
 
   post '/delete/purchase' do
     purchase_id = params[:id].to_i
-    if purchase_id.to_s != params[:id]
-      return {:success => false, :error => "Could not parse purchase ID", :value => purchase_id}
-    end
-
     purchase = Purchase.first(:id => purchase_id)
     if !purchase.destroy
       return {:success => false, :error => "Could not destroy purchase", :value => purchase_id}
